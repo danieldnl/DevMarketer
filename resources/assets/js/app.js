4c01f2de2f3d91d39440727a9b61d54ee737116a
Daniel Carneiro Miranda <daniel.c.miranda@terra.com.br>
2017-07-24 18:49:12 -0300

    Ep. 8 - Styling the Top Nav

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,3 +6,13 @@ import Buefy from 'buefy'
 
 Vue.use(Buefy);
 
+var app = new Vue({
+    el:'#app',
+    data: {}
+})
+
+$(document).ready(function(){
+    $('button.dropdown').hover(function(e){
+        $(this).toggleClass('is-open');
+    });
+});
