9181511ba6c31bb3a7d27082b8ca899618106f65
Daniel Carneiro Miranda <daniel.c.miranda@terra.com.br>
2017-07-31 03:56:13 -0300

    Ep 9 - Register and Login Form Styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,14 +13,20 @@
                         <div class="field">
                             <label for="email" class="label">Email Address</label>
                             <p class="control">
-                                <input class="input" type="text" name="email" id="email" placeholder="name@example.com">
+                                <input class="input {{ $errors->has('email') ? 'is-danger' : '' }}" type="text" name="email" id="email" placeholder="name@example.com" value="{{ old('email') }}">
                             </p>
+                            @if($errors->has('email'))
+                                <p class="help is-danger">{{ $errors->first('') }}</p>
+                            @endif
                         </div>
                         <div class="field">
                             <label for="password" class="label">Password</label>
                             <p class="control">
-                                <input class="input" type="password" name="password" id="password">
+                                <input class="input {{ $errors->has('password') ? 'is-danger' : '' }}" type="password" name="password" id="password">
                             </p>
+                            @if($errors->has('password'))
+                                <p class="help is-danger">{{ $errors->first('password') }}</p>
+                            @endif
                         </div>
 
                         <b-checkbox name="remember" class="m-t-20">Remember Me</b-checkbox>
@@ -28,7 +34,8 @@
                         <button class="button is-primary is-outlined is-fullwidth m-t-30">Log In</button>
                     </form>
                 </div>{{-- End of card-content--}}
-            </div>
+            </div> {{-- End of Card--}}
+            <h5 class="has-text-centered m-t-10"><a href="{{ route('password.request') }}" class="is-muted">Forgot Your Password?</a></h5>
         </div>
     </div>
 
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+    @if (session('status'))
+        <div class="notification is-success">
+            {{ session('status') }}
+        </div>
+    @endif
+
+    <div class="columns m-b-10">
+        <div class="column is-one-third is-offset-one-third m-t-100">
+            <div class="card">
+
+                <div class="card-content">
+                    <h1 class="title">Forgot Password?</h1>
+                    <form action="{{ route('password.email') }}" method="POST" role="form">
+                        {{ csrf_field() }}
+                        <div class="field">
+                            <label for="email" class="label">Email Address</label>
+                            <p class="control">
+                                <input class="input {{ $errors->has('email') ? 'is-danger' : '' }}" type="text"
+                                       name="email" id="email" placeholder="name@example.com" value="{{ old('email') }}" required>
+                            </p>
+                            @if($errors->has('email'))
+                                <p class="help is-danger">{{ $errors->first('email') }}</p>
+                            @endif
+                        </div>
+
+                        <button class="button is-primary is-outlined is-fullwidth m-t-30">Get Reset Link</button>
+                    </form>
+                </div>{{-- End of card-content--}}
+            </div> {{-- End of Card--}}
+            <h5 class="has-text-centered m-t-10"><a href="{{ route('login') }}" class="is-muted">Back to Login</a></h5>
+        </div>
+    </div>
+{{--<div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -42,5 +75,5 @@
             </div>
         </div>
     </div>
-</div>
+</div>--}}
 @endsection
